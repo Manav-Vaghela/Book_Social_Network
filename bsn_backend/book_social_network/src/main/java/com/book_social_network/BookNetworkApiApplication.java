d63ee6af7acdae18745f044b6e93e086a23a1742
Manav <vaghelamanav2320@gmail.com>
2025-10-29 19:22:58 +0530

    Improved Registration Process

--- bsn_backend/book_social_network/src/main/java/com/book_social_network/BookNetworkApiApplication.java
+++ bsn_backend/book_social_network/src/main/java/com/book_social_network/BookNetworkApiApplication.java
@@ -1,17 +1,34 @@
 package com.book_social_network;
 
+import com.book_social_network.Entity.Role;
+import com.book_social_network.Repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class BookNetworkApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookNetworkApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(RoleRepository roleRepository){
+
+		return args -> {
+
+			if(roleRepository.findByName("USER").isEmpty()){
+
+				roleRepository.save(Role.builder().name("USER").build());
+			}
+		};
+	}
 }
