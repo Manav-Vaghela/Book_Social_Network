ef683c88fe5cf1d2fc2ad3bb86c7948ff13f6463
Manav <vaghelamanav2320@gmail.com>
2025-10-29 19:21:58 +0530

    Created Registration Process

---
+++ bsn_backend/book_social_network/src/main/java/com/book_social_network/Controllers/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.book_social_network.Controllers;
+
+import com.book_social_network.Entity.RegistrationRequest;
+import com.book_social_network.Service.AuthenticationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication")
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
+
+        authenticationService.register(request);
+        return ResponseEntity.accepted().build();
+    }
+}
