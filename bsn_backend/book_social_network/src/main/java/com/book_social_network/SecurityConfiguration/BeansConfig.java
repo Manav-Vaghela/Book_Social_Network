534fda82eab25eb876109bc0a6faac78ccf4ac60
Manav <vaghelamanav2320@gmail.com>
2025-11-01 21:20:14 +0530

    Implemented AuditAware Functionality

--- bsn_backend/book_social_network/src/main/java/com/book_social_network/SecurityConfiguration/BeansConfig.java
+++ bsn_backend/book_social_network/src/main/java/com/book_social_network/SecurityConfiguration/BeansConfig.java
@@ -3,6 +3,7 @@ package com.book_social_network.SecurityConfiguration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -36,5 +37,11 @@ public class BeansConfig {
             throws Exception {
 
         return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuditorAware<Integer> auditorAware() {
+
+        return new ApplicationAuditAware();
     }
 }
