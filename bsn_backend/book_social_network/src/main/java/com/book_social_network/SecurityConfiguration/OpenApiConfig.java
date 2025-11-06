8389fdf5244485f21853c93d3847d326ec4fe68e
Manav <vaghelamanav2320@gmail.com>
2025-11-06 22:40:22 +0530

    Implemented API Documentation(Swagger)

---
+++ bsn_backend/book_social_network/src/main/java/com/book_social_network/SecurityConfiguration/OpenApiConfig.java
@@ -0,0 +1,58 @@
+package com.book_social_network.SecurityConfiguration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.stereotype.Service;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Bool_Social_Network",
+                        email = "xyb@abc.com",
+                        url = "https://test.com"
+                ),
+                description = "OpenApi documentation for spring security",
+                title = "OpenApi specification - xyz",
+                version = "1.0",
+                license = @License(
+                        name = "Licence name",
+                        url = "https://licence.com"
+
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+                @Server(
+                        description = "Prod ENV",
+                        url = "https://test.com"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+
+
+}
