1a279942b478f7391b3068c065fd8ca8c6054ca3
Manav <vaghelamanav2320@gmail.com>
2025-11-15 19:27:19 +0530

    Added new route for register

--- bsn_frontend/book-network-ui/src/app/app.routes.ts
+++ bsn_frontend/book-network-ui/src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
 
 export const routes: Routes = [
 
     {
         path: 'login',
         component: LoginComponent
-    }
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
 ];
