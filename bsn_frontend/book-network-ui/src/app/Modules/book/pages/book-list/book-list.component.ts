12ddab2b5b0e8fd58a54977c25a61ae0b5a39418
Manav <vaghelamanav2320@gmail.com>
2025-11-27 23:40:21 +0530

    Implemented pagination functionality

--- bsn_frontend/book-network-ui/src/app/Modules/book/pages/book-list/book-list.component.ts
+++ bsn_frontend/book-network-ui/src/app/Modules/book/pages/book-list/book-list.component.ts
@@ -2,41 +2,106 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiConfiguration } from '../../../../services/api-configuration';
 import { HttpClient } from '@angular/common/http';
-import { findAllBooks } from '../../../../services/functions';
-import { PageResponseBookResponse } from '../../../../services/models';
-import { BookCardComponent } from "../../components/book-card/book-card.component";
+import { borrowBook, findAllBooks } from '../../../../services/functions';
+import {
+  BookResponse,
+  PageResponseBookResponse,
+} from '../../../../services/models';
+import { BookCardComponent } from '../../components/book-card/book-card.component';
 
 @Component({
   selector: 'app-book-list',
   imports: [BookCardComponent],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.scss'
+  styleUrl: './book-list.component.scss',
 })
-export class BookListComponent implements OnInit{
+export class BookListComponent implements OnInit {
+  bookResponse: PageResponseBookResponse = {};
+  page = 0;
+  size = 5;
+  message: string = '';
+  level = 'success';
 
-    bookResponse: PageResponseBookResponse = {};
-    page = 0;
-    size = 5;
+  constructor(
+    private router: Router,
+    private apiConfig: ApiConfiguration,
+    private httpClient: HttpClient
+  ) {}
 
-    constructor(
-      private router: Router,
-      private apiConfig: ApiConfiguration,
-      private httpClient: HttpClient
-    ){}
+  ngOnInit(): void {
+    this.findAllBooks();
+  }
 
-    ngOnInit(): void {
-        
-      this.findAllBooks();
-    }
+  private findAllBooks() {
+    findAllBooks(this.httpClient, this.apiConfig.rootUrl, {
+      page: this.page,
+      size: this.size,
+    }).subscribe({
+      next: (result) => {
+        this.bookResponse = result.body;
+      },
+    });
+  }
 
-    private findAllBooks(){
+  get isLastPage(): boolean {
+    return this.page == (this.bookResponse.totalPages as number) - 1;
+  }
 
-      findAllBooks(this.httpClient , this.apiConfig.rootUrl , {page: this.page ,size: this.size }).subscribe({
+  goToFirstPage() {
+    this.page = 0;
+    this.findAllBooks();
+  }
 
-          next: (result)=>{
+  goToPreviousPage() {
+    this.page--;
+    this.findAllBooks();
+  }
 
-              this.bookResponse = result.body;
+  goToPage(page: number) {
+    this.page = page;
+    this.findAllBooks();
+  }
+
+  goToNextPage() {
+    this.page++;
+    this.findAllBooks();
+  }
+
+  goToLastPage() {
+    this.page = (this.bookResponse.totalPages as number) - 1;
+    this.findAllBooks();
+  }
+
+  async borrowBook(book: BookResponse): Promise<void> {
+
+    this.message = '';
+    borrowBook(this.httpClient, this.apiConfig.rootUrl, {
+      'book-id': book.id as number,
+    }).subscribe({
+      next: () => {
+        this.level = 'success';
+        this.message = 'Book successfully added to your list';
+      },
+
+      error: async (err) => {
+        let errorObj: any = err.error;
+
+        console.log(err);
+        this.level = 'error';
+
+        if(err.error instanceof Blob) {
+          const text = await err.error.text(); // convert Blob → text
+          try {
+            const json = JSON.parse(text); // convert text → JSON
+            this.message = json.error;
+          } catch {
+            this.message = text; 
           }
-      })
-    }
+        }
+        else {
+          this.message = err.error?.error || err.message;
+        }
+      },
+    });
+  }
 }
